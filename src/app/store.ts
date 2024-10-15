98d2cb849047a25cd857b2dd04252678c294aab5
Sa-Yoor Headley <sai.h152015@gmail.com>
2024-10-15 18:48:07 -0400

    boilerplate

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 
 export const store = configureStore({
-  reducer: { counter: counterReducer },
+  reducer: { },
 });
 
 // Infer the `RootState`,  `AppDispatch`, and `AppStore` types from the store itself
